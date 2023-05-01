ac8884d2c95c4e1b9c3991e2d8d6578c8fde8e5c
DawRad <dawidissimo@gmail.com>
2023-05-01 22:29:41 +0200

    Dodanie licznika rodzajów zasobów do klasy "Zasob".

--- src/Fundamenty.py
+++ src/Fundamenty.py
@@ -6,10 +6,19 @@ class Obiekt:
         return "Numer porzadkowy: " + str(self.nrPorz_)
 
 class Zasob(Obiekt):
-    def __init__(self, idx: int, rodzaj: str, kwota: float) -> None:
-        super(idx)
+    __liczObZas_ = dict()
+
+    def __init__(self, rodzaj: str, kwota: float) -> None:
+        if self.__liczObZas_.get(rodzaj) != None: self.__liczObZas_[rodzaj] += 1
+        else: self.__liczObZas_[rodzaj] = 1
+        super().__init__(self.__liczObZas_[rodzaj])
+
         self.rodzaj_ = rodzaj
-        self.kwota_ = kwota
+        self.kwota_ = kwota        
 
     def __str__(self) -> str:
         return super().__str__() + "\nRodzaj zasobu: " + self.rodzaj_ + "\n" + "Dostepne srodki: " + str(self.kwota_)
+    
+    @classmethod
+    def resetujLicznikZasobow(cls):
+        Zasob.__liczObZas_ = dict()
