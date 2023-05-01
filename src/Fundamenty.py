44ae3f3980c0ce33479b3cf4fd93cf8cc8429950
DawRad <dawidissimo@gmail.com>
2023-05-02 01:33:04 +0200

    Utworzenie klas "Wydatek" oraz "Paragon", dodanie metody aktualizującej pole kwoty klasy "Zasob" i inne zmiany.

--- src/Fundamenty.py
+++ src/Fundamenty.py
@@ -1,23 +1,42 @@
+import datetime as dtm
+
+
+"""
+| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - \
+| - - - - - ***Klasa Obiekt*** - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - /
+"""
 class Obiekt:
     def __init__(self, idx: int) -> None:
         self.nrPorz_ = idx
 
     def __str__(self) -> str:
-        return "Numer porzadkowy: " + str(self.nrPorz_)
+        return "Indeks: " + str(self.nrPorz_)
 
+
+"""
+| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - \
+| - - - - - ***Klasa Zasob*** - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - /
+"""
 class Zasob(Obiekt):
     __liczObZas_ = dict()
 
-    def __init__(self, rodzaj: str, kwota: float) -> None:
+    def __init__(self, rodzaj: str, kwota: float, waluta) -> None:
         if self.__liczObZas_.get(rodzaj) != None: self.__liczObZas_[rodzaj] += 1
         else: self.__liczObZas_[rodzaj] = 1
         super().__init__(self.__liczObZas_[rodzaj])
 
-        self.rodzaj_ = rodzaj
-        self.kwota_ = kwota        
+        self.__rodzaj_ = rodzaj
+        self.__kwota_ = kwota
+        self.__waluta_ = waluta
 
     def __str__(self) -> str:
-        return super().__str__() + "\nRodzaj zasobu: " + self.rodzaj_ + "\n" + "Dostepne srodki: " + str(self.kwota_)
+        return super().__str__() + "\nRodzaj zasobu: " + self.__rodzaj_ + "\n" + "Dostepne srodki: " + str(self.__kwota_)
+    
+    # W argumencie "kwota" znak musi uwzględniać czy pole klasy jest zmniejszane lub powiększane
+    def aktualizujKwote(self, kwota):
+        self.__kwota_ += kwota
     
     @classmethod
     def resetujLicznikZasobow(cls):
@@ -30,4 +49,51 @@ class Zasob(Obiekt):
 
     @classmethod
     def wypiszRodzajeZasobow(cls):
-        for [klucz, wart] in Zasob.__liczObZas_.items(): print("Rodzaj: ", klucz, "\nLiczba instancji: ", wart, "\n")            
+        for [klucz, wart] in Zasob.__liczObZas_.items(): print("Rodzaj: ", klucz, "\nLiczba instancji: ", wart, "\n")
+
+
+"""
+| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - \
+| - - - - - ***Klasa Wydatek*** - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - /
+"""
+class Wydatek(Obiekt):
+    def __init__(self, idx: int, cena: float, nazwa = "inne", dzial = "inny") -> None:
+        super().__init__(idx)
+        self.__cena_ = cena
+        self.__dzial_ = dzial
+        self.__nazwa_ = nazwa
+
+    def __str__(self) -> str:
+        return super().__str__() + "\nDział: " + str(self.__dzial_) + "\nNazwa: " + self.__nazwa_ + "\nCena: " + str(self.__cena_)
+    
+    def podajCene(self) -> float:
+        return self.__cena_
+
+
+"""
+| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - \
+| - - - - - ***Klasa Paragon*** - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - /
+"""
+class Paragon(Obiekt):
+    def __init__(self, idx: int, data: dtm.date, miejsce = "nieznane", pozycje = [Wydatek(1, 999.99)]) -> None:
+        super().__init__(idx)
+        self.__data_ = data
+        self.__miejsce_ = miejsce
+        self.__pozycje_ = pozycje
+
+    def __str__(self) -> str:
+        lista_pozycji = str("")
+        for pozycja in self.__pozycje_: lista_pozycji += pozycja.__str__() + "\n- - - - -\n"
+        lista_pozycji = lista_pozycji.removesuffix("\n- - - - -\n")
+
+        return super().__str__() + "\nData: " + self.__data_.__str__() + "\nMiejsce: " + self.__miejsce_ + "\n\nLista wydatków:\n" + lista_pozycji
+    
+    def dodajPozycje(self, pozycja: Wydatek) -> None:
+        self.__pozycje_.append(pozycja)
+
+    def sumujWydatki(self) -> float:
+        suma = 0.0
+        for wydatek in self.__pozycje_: suma += wydatek.podajCene()
+        return suma
