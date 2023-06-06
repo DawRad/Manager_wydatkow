1f9da39e4c6b4d5cb399eaa4de0796b4d3b13f5e
DawRad <dawidissimo@gmail.com>
2023-06-06 11:17:55 +0200

    Dodanie nowych funkcji w klasie "Interfejs".

--- src/Interfejs.py
+++ src/Interfejs.py
@@ -4,5 +4,13 @@ class Interfejs:
     def __init__(self) -> None:
         self.__posiadacze_ = list(Posiadacz)
     
-    def utworzPosiadacza(self, idx = 1, imie = "Jan", nazwisko = "Kowalski"):
-        self.__posiadacze_.append(Posiadacz(idx, imie, nazwisko))
+    def utworzPosiadacza(self, imie = "Jan", nazwisko = "Kowalski"):
+        self.__posiadacze_.append(Posiadacz(imie, nazwisko))
+
+    def dodajTabWydatki(self, posiadacz_hash, nazwa: str, kolumny: list(str)):
+        for posiadacz in self.__posiadacze_:
+            if posiadacz.podajHash() == posiadacz_hash:
+                nowa_tab = TabWydatki(nazwa, kolumny)
+                posiadacz.dodajTab(nowa_tab)
+
+         
