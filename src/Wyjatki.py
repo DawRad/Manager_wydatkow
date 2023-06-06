a8d57fc364444fbfd3b3e136b262fc5d62de582d
DawRad <dawidissimo@gmail.com>
2023-06-06 17:56:23 +0200

    Dodanie nowego wyjątku: rozszerzenia plików.

--- src/Wyjatki.py
+++ src/Wyjatki.py
@@ -1,3 +1,7 @@
 class OutOfBondsError(ValueError):
     def __init__(self, message):
         super().__init__(message)
+
+class BadFileFormatError(ValueError):
+    def __init__(self, message) -> None:
+        super().__init__(message)
