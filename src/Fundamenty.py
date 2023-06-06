9c7808b6d564a274387e1218f5748d8d433b1c9a
DawRad <dawidissimo@gmail.com>
2023-06-06 17:58:58 +0200

    Implementacja nowych metod w klasach "TabWydatki" (operacje na polu "tabela_") i "Posiadacz" (m. in. dołączanie pandas.DataFrame do obiektów klasy "TabWydatki").

--- src/Fundamenty.py
+++ src/Fundamenty.py
@@ -60,16 +60,27 @@ class Zasob(Obiekt):
 """
 
 class TabWydatki(Obiekt):
-    def __init__(self, nazwa: str, kolumny = ["Sklep", "Data", "Towar", "Cena"], wart_domyslne = ("Nieznany", dtm.date.today(), "pieczywo", 4.4)) -> None:
+    '''
+    Parametr df -> parametr ty pandas.DataFrame
+    '''
+    def __init__(self, nazwa: str, kolumny = ["Sklep", "Data", "Towar", "Cena"], df: pd.DataFrame = None, wart_domyslne = ("Nieznany", dtm.date.today(), "pieczywo", 4.4)) -> None:
         super().__init__()
         self.__nazwa_ = nazwa
-        self.__tabela_ = pd.DataFrame(columns=kolumny)        
+
+        if df != None: self.__tabela_ = df
+        else: self.__tabela_ = pd.DataFrame(columns=kolumny)      
 
     def podajNazwyKol(self):
         return self.__tabela_.columns
     
     def podajNazwe(self):
         return self.__nazwa_
+    
+    def podajDF(self):
+        return self.__tabela_
+    
+    def dolaczDF(self, new_df):
+        self.__tabela_ = pd.concat([self.__tabela_, new_df], ignore_index=True)
     
 
 """
@@ -80,26 +91,47 @@ class TabWydatki(Obiekt):
 class Posiadacz(Obiekt):
     def __init__(self, imie: str, nazwisko: str) -> None:
         super().__init__()
-        self.__tabWydatki_ = list()
+        self.__tabWydatki_ = []
         self.__imie_ = imie
         self.__nazwisko_ = nazwisko
-        self.__zasoby_ = list()
+        self.__zasoby_ = []
         self.__hash_ = (self.__imie_ + self.__nazwisko_).lower()
 
     def podajHash(self):
         return self.__hash_
     
+    def podajTabDF(self, nazwa_tab: str):
+        for tabela in self.__tabWydatki_:
+            if tabela.podajNazwe() == nazwa_tab: return tabela.podajDF()
+
+        return pd.DataFrame({'A' : []})
+    
+    def podajTab(self, nazwa_tab: str):
+        for tabela in self.__tabWydatki_:
+            if tabela.podajNazwe() == nazwa_tab: return tabela
+
+        return None
+    
     def dodajTab(self, tabela: TabWydatki):
         self.__tabWydatki_.append(tabela)
 
-    def dodajWierszWTab(self, new_row: list(), nazwa_tab = " "):
+    def dodajTabDF(self, nazwa_tab: str, df: pd.DataFrame):
+        new_tab = TabWydatki(nazwa_tab, df=df)
+        self.__tabWydatki_.append(new_tab)
+
+    def dodajWierszWTab(self, new_row: list, nazwa_tab = " "):
         for tabela in self.__tabWydatki_:
             if tabela.podajNazwe() == nazwa_tab:
-                target = tabela
+                targetTab = tabela.podajDF()
                 break
 
         # target = pd.concat([target, new_row.to_frame().T], ignore_index=True)
-        target.loc[len(target)] = new_row
+        targetTab.loc[len(targetTab)] = new_row
+
+    def dolaczDoTab(self, nazwa_tab, new_df):
+        target = self.podajTab(nazwa_tab)
+        if target == None: pass
+        else: target.dolaczDF(new_df)
 
     def podajNazwyKolWTab(self, nazwa: str):
         for tab in self.__tabWydatki_:
