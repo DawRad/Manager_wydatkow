ba30f6ae6f637bdbe4b09175f357d8076ae6466f
DawRad <dawidissimo@gmail.com>
2023-06-27 17:32:30 +0200

    Dodanie nowych wyjątków: "MissingKeyError" oraz "BadDFCols"

--- src/Wyjatki.py
+++ src/Wyjatki.py
@@ -6,6 +6,14 @@ class BadFileFormatError(ValueError):
     def __init__(self, message) -> None:
         super().__init__(message)
 
+class MissingKeyError(ValueError):
+    def __init__(self, message: object) -> None:
+        super().__init__(message)
+
 class RepKeyError(ValueError):
     def __init__(self, message) -> None:
+        super().__init__(message)
+
+class BadDFCols(ValueError):
+    def __init__(self, message: object) -> None:
         super().__init__(message)
